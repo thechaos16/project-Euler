cc8c017dcb5c9a1cb11e0b911c0c7e4df78bf60c
Minkyu Kim <thechaos16@gmail.com>
2016-01-12 22:02:33 +0900

    nothing much

--- python/357th_notcompleted.py
+++ python/357th_notcompleted.py
@@ -1,8 +1,6 @@
 import math
 
-pri = [2,3,5,7,11,13];
-
-def isprime(n):
+def isprime(n,pri):
     if n in pri:
         return 1
     for i in range(len(pri)):
@@ -11,22 +9,30 @@ def isprime(n):
     for i in range(2,int(math.sqrt(n))+1):
         if n%i==0:
             return 0
-    pri.append(n)
+    #pri.append(n)
     return 1
+
+def primeLessThan(n):
+    #priList = [2]
+    priList = []
+    for i in range(2,n+1):
+        if isprime(i,priList):
+            priList.append(i)
+    return priList
 
 def divisors(n):
     div = []
-    for i in range(2,int(math.sqrt(n))+1):
+    for i in range(1,int(math.sqrt(n))+1):
         if n%i==0:
             if i not in div:
                 div.append(i+n/i)
     return div
 
-def checker(n):
+def checker(n,priList):
     div = divisors(n)
     dd=0
     for i in range(len(div)):
-        if isprime(div[i])==0:
+        if isprime(div[i],priList)==0:
             dd=1
             break
     if dd==0:
@@ -34,8 +40,14 @@ def checker(n):
     return 0
 
 nn = []
-for i in range(2,100000000):
-    if checker(i)==1:
-        nn.append(i)
+#for i in range(2,100000000):
+ #   if checker(i)==1:
+  #      nn.append(i)
 
-        
+priList = primeLessThan(100000000)
+print('list is finished')
+cnt=0
+for prime in priList:
+    if checker(prime-1,priList):
+        #print(prime-1)
+        cnt+=prime-1
