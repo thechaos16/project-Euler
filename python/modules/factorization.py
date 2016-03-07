a98805ab3ac7477559999f8b6e777849c31acc2c
minkyu <mkkim@solidware.io>
2016-03-07 11:11:27 +0900

    module updated

--- python/modules/factorization.py
+++ python/modules/factorization.py
@@ -11,22 +11,38 @@ try:
 except ImportError:
     sys.path.append('../')
     import modules.prime_handler as ph
-    
 
-def factorization(n):
+def update_factor_dictionary(prev_dict,new_dict):
+    keys = list(new_dict)
+    for key in keys:
+        if key not in prev_dict:
+            prev_dict[key] = new_dict[key].copy()
+    return prev_dict
+
+def factorization(n,factor_dict={}):
+    if int(n) in factor_dict:
+        return [factor_dict[int(n)],factor_dict]
     phi = ph.PrimeHandler(n)
     prime_list = phi.get_prime_list()
     if n in prime_list:
-        return {int(n):1}
+        factor_dict = update_factor_dictionary(factor_dict,{int(n):{int(n):1}})
+        return [{int(n):1},factor_dict]
     return_dict = {}
     for prime in prime_list:
         if n%prime==0:
-            return_dict = factorization(n/prime)
+            if int(n/prime) in factor_dict:
+                return_dict = factor_dict[int(n/prime)].copy()
+            else:
+                [return_dict, new_factor_dict] = factorization(n/prime,factor_dict)
+                factor_dict = update_factor_dictionary(factor_dict,new_factor_dict)
             if int(prime) in list(return_dict):
                 return_dict[int(prime)]+=1
             else:
                 return_dict[int(prime)]=1
-            return return_dict
+            factor_dict[int(n)]=return_dict.copy()
+            return [return_dict, factor_dict]
     
 if __name__=='__main__':
-    print(factorization(14))
+    factor_dict = {}
+    for i in range(2,10):
+        [aa, factor_dict] = factorization(i,factor_dict)
--- python/ongoing/179th.py
+++ python/ongoing/179th.py
@@ -16,34 +16,35 @@ def run():
     cnt = 0
     i=2
     prev_fact_num = None
+    factor_dict = {}
     while True:
-        if i>10e7:
+        if i>1000:
             break
-        [result,prev_fact_num] = check_factor_number(i,prev_fact_num)
+        [result,prev_fact_num,factor_dict] = check_factor_number(i,prev_fact_num,factor_dict)
         if result:
             #print(i)
             cnt+=1            
         i+=1
     return cnt
     
-def check_factor_number(n,prev_fact_num=None):
+def check_factor_number(n,prev_fact_num=None,factor_dict = {}):
     if n==1:
-        return False
+        return [False,factor_dict]
     if prev_fact_num is None:
-        prev_fact_num = factor_number(n-1)
-    cur_fact_num = factor_number(n)
-    return [cur_fact_num==prev_fact_num,cur_fact_num]
+        [prev_fact_num,factor_dict] = factor_number(n-1,factor_dict)
+    [cur_fact_num,factor_dict] = factor_number(n,factor_dict)
+    return [cur_fact_num==prev_fact_num,cur_fact_num,factor_dict]
         
-def factor_number(n):
+def factor_number(n,factor_dict):
     if n<=0:
-        return 0
+        return [0,factor_dict]
     elif n==1:
-        return 1
-    factorization = factor.factorization(n)
+        return [1,factor_dict]
+    [factorization,factor_dict] = factor.factorization(n,factor_dict)
     ans = 1.0
     for key in list(factorization):
         ans*=(factorization[key]+1)
-    return ans
+    return [ans,factor_dict]
     
 if __name__=='__main__':
     print(run())
